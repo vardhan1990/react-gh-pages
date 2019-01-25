918f818ecc2a168829d0aade056e5dd0aef84a9f
Vardhan Dugar <vardhan1990@gmail.com>
2019-01-24 23:02:05 -0800

    make webpage scrollable

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ class App extends Component {
   render() {
     return (
       <div className="App-sections">
-        <img src={ProfilePic} className="App-section1" />
+        <img src={ProfilePic} alt-text="Profile Picture" className="App-section1" />
         <header className="App-section2">
           <h1 className="header">Vardhan Dugar</h1>
           <h3 className="text">I live in Seattle.</h3>
           <h3 className="text">I write code for a living.</h3>
-          <h3 className="text">I like to ski, run, swim and hike.</h3>
+          <h3 className="text">I like to ski, run, raft, swim and hike.</h3>
           <h3 className="text">I like good food and good board games.</h3>
           <h3 className="text">I walk dogs. Others', not my own. I am not adult enough to have my own pets.</h3>
           <h3 className="text">I will add more content to this site to make it worth your while.</h3>
